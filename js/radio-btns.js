5cdb5d06396c819056b7aad2d914c4f1ff74e842
Mozzhakov <imozzhakov@icloud.com>
2023-05-10 21:45:26 +0300

    added radio-buttons filter

---
+++ js/radio-btns.js
@@ -0,0 +1,19 @@
+const cardList = document.querySelector('.card-set');
+const allCards = document.querySelectorAll('.card-set__item');
+const btnList = document.querySelector('.buttons-list');
+
+const allCardsArr = Array.from(allCards);
+
+btnList.addEventListener('click', changeList);
+
+function changeList(e) {
+  if (e.target.nodeName !== 'BUTTON') return;
+
+  allCards.forEach(el => el.remove());
+  const neededEls = allCardsArr.filter(el => el.dataset.type === e.target.dataset.type);
+  neededEls.map(el => cardList.appendChild(el));
+
+  if (e.target.dataset.type === 'all') {
+    allCardsArr.map(el => cardList.appendChild(el));
+  }
+}
